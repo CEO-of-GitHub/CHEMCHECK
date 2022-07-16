c7748d23e8d9c13ca17e5d1c0de4be892c58d657
quaaa_ <88337196+CEO-of-GitHub@users.noreply.github.com>
2022-07-16 11:34:09 +0800

    Update prototype_calibration.py

--- prototype_calibration.py
+++ prototype_calibration.py
@@ -8,6 +8,10 @@ s3 = 24
 signal = 25
 NUM_CYCLES = 10
 
+nRED = 0
+nGREEN = 1
+nBLUE = 2
+
 def setup():
   GPIO.setmode(GPIO.BCM)
   GPIO.setup(signal,GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -16,7 +20,7 @@ def setup():
   GPIO.setup(s2,GPIO.OUT)
   GPIO.setup(s3,GPIO.OUT)
   
-  GPIO.output(s0,GPIO.HIGH)
+  GPIO.output(s0,GPIO.LOW)
   GPIO.output(s1,GPIO.HIGH)
   
   print("\n")
@@ -26,14 +30,38 @@ def loop():
   while(1): 
        
     print("Start...")
-    time.sleep(5) 
+    time.sleep(5)
     
-    RAW_2 = colorCalibrate(GPIO.LOW,GPIO.LOW,"R",255)
-    RAW_1 = colorCalibrate(GPIO.LOW,GPIO.LOW,"R",0)
+    
+    # Calibrate for (255,0,0), (0,255,0), and (0,0,255)
+    print("Provide RGB model for RED (255,0,0)")
+    red_high = colorCalibrate(GPIO.LOW,GPIO.LOW,'R',255)
+    print("Provide RGB model for GREEN (0,255,0)")
+    green_high = colorCalibrate(GPIO.HIGH,GPIO.HIGH,'G',255)
+    print("Provide RGB model for BLUE (0,0,255)")
+    blue_high = colorCalibrate(GPIO.LOW,GPIO.HIGH,'B',255)
+    calibration_high = [red_high, green_high, blue_high]
+    
+    # Calibrate for (0,0,0)
+    print("Provide RGB model for BLACK (0,0,0)")
+    red_low = colorCalibrate(GPIO.LOW,GPIO.LOW,'R',0)
+    green_low = colorCalibrate(GPIO.HIGH,GPIO.HIGH,'G',0)
+    blue_low = colorCalibrate(GPIO.LOW,GPIO.HIGH,'B',0)
+    calibration_low = [red_low, green_low, blue_low]
+    
+    # Find RAW color values of sample
     print("Provide sample:")
-    time.sleep(5) 
-    RAWval = colorReadRAW(GPIO.LOW,GPIO.LOW)
-    print("(r)" + str(colorRead(RAWval, RAW_1, RAW_2, 0, 255)))
+    time.sleep(5)
+    RAWval = colorReadRAW_FULL()
+    
+    RGB_1 = [0,0,0]
+    RGB_2 = [255,255,255]
+    
+    # Determine each of the RGB (all red, green, and blue) values of sample based on previous calibration
+    print("Processing...")
+    time.sleep(5)
+    print(str(colorRead_FULL(RAWval, calibration_low, calibration_high, RGB_1, RGB_2)))
+    # Needs revision
     
     # RED
     #print("Red value - " + str(colorReadRAW(GPIO.LOW,GPIO.LOW)))
@@ -59,6 +87,20 @@ def colorCalibrate(s2val,s3val,color,intensity):
     # 'intensity' refers to intensity value of R, G, or B hue, which falls in the range of 0-255 
     return calibration_value
     # 'calibration_value' refers to RAW intensity value
+
+def colorCalibrate_FULL():
+    time.sleep(5)
+    print("Calibrating the RED value:")
+    time.sleep(1)
+    red = colorReadRAW(GPIO.LOW,GPIO.LOW)
+    print("Calibrating the GREEN value:")
+    time.sleep(1)
+    green = colorReadRAW(GPIO.HIGH,GPIO.HIGH)
+    print("Calibrating the BLUE value:")
+    time.sleep(1)
+    blue = colorReadRAW(GPIO.LOW,GPIO.HIGH)
+    calibration_value = [red, green, blue]
+    return calibration_value
     
 def colorReadRAW(s2val,s3val):
     GPIO.output(s2,s2val)
@@ -75,9 +117,23 @@ def colorReadRAW(s2val,s3val):
     result_value = NUM_CYCLES / duration
     return result_value    
     
+def colorReadRAW_FULL():
+    red = colorReadRAW(GPIO.LOW,GPIO.LOW)
+    green = colorReadRAW(GPIO.HIGH,GPIO.HIGH)
+    blue = colorReadRAW(GPIO.LOW,GPIO.HIGH)
+    RAWlist = [red, green, blue]
+    return RAWlist
+    
 def colorRead(RAWval, RAW_1, RAW_2, RGB_1, RGB_2):
     return int((RAWval) * ((RGB_2 - RGB_1) / (RAW_2 - RAW_1)))
     # above equation is just a Python version of Arduino's map() function
+
+def colorRead_FULL(RAWval, RAW_1, RAW_2, RGB_1, RGB_2):
+    red = int((RAWval[nRED]) * ((RGB_2[nRED] - RGB_1[nRED]) / (RAW_2[nRED] - RAW_1[nRED])))
+    green = int((RAWval[nGREEN]) * ((RGB_2[nGREEN] - RGB_1[nGREEN]) / (RAW_2[nGREEN] - RAW_1[nGREEN])))
+    blue = int((RAWval[nBLUE]) * ((RGB_2[nBLUE] - RGB_1[nBLUE]) / (RAW_2[nBLUE] - RAW_1[nBLUE])))
+    RGBlist = [red, green, blue]
+    return RGBlist
 
 def endprogram():
     GPIO.cleanup()
@@ -102,3 +158,5 @@ if __name__=='__main__':
 # For CHEMCHECK's sample detection, 3D print a "sample module". Similar sa IMAHE stackable sample holder so that non-reflective material can be locked in place (instead of nylon mesh kay this time it's the transparent material)
 
 # Unify calibration for R, G, and B tomorrow
+
+# Order of functions in calibration process: (colorCalibrate && colorReadRAW >> colorRead) or (colorCalibrate_FULL && colorReadRAW_FULL >> colorRead_FULL)
